e23e2134b333c96162e58a5de853e4b1619308b0
Gautam Singh <lossangle20@gmail.com>
2023-11-26 08:38:56 +0530

    add

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './style.module.css'
 import { useNavigate } from 'react-router-dom';
 
 export function Header() {
     const navigate = useNavigate();
-    const [searchInput, setSearchInput] = useState("");
 
     function handleLogo() {
         navigate("/");
@@ -14,20 +13,9 @@ export function Header() {
         if (type === "TV") navigate('./explore/tv');
     }
 
-    function searchQueryHandler(e) {
-        if (e.key === "Enter" && searchInput.length > 0) {
-          navigate(`/search/${searchInput}`);
-        }
-        else {
-          if (e.key === "Enter" && searchInput.length === 0)
-            return alert("Empty Input Field!");
-        }
-      }
+
       function handleSubmit() {
-        if (searchInput.length === 0)
-          return alert("Empty Input Field!");
-        else
-          navigate(`/search/${searchInput}`);
+          navigate(`/`);
       }
 
     return (
@@ -44,10 +32,8 @@ export function Header() {
                     <div className="navbar-nav">
                         <button className={style['navbarButtons']} onClick={() => handlerMovieOrTv("Movie")}>Movies</button>
                         <button className={style['navbarButtons']} onClick={() => handlerMovieOrTv("TV")}>TV Shows</button>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} onKeyUp={searchQueryHandler}/>
-                            <button className={style['navbarButtons']} onClick={handleSubmit}>Search</button>
-                        </form>
+                        <button className={style['navbarButtons']} onClick={handleSubmit}>Search</button>
+                    
                     </div>
 
                 </div>
